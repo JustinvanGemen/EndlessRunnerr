f26ece7b415d3b01ed3e14fc53e83995e40dbc96
jvdwijk <joeyvdwijk@gmail.com>
2016-09-16 11:21:11 +0200

    update

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthSystem : MonoBehaviour {
+
+    private float lifes = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (lifes <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerHit(Collision other)
+    {
+        if (CompareTag("Obstacle"))
+        {
+            lifes = lifes - 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+    private float timeToMove = 0.1F;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveRight();
+    }
+    private void MoveRight()
+    {
+        if (Time.time > timeToMove)
+        {
+            Vector3 position = this.transform.position;
+            position.x +2;
+            this.transform.position = position;
+            timeToMove = 0.2f + Time.time;
+        }
+    }
+}
