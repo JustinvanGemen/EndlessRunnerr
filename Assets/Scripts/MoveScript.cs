e920ee338a6465a8738696bd5247dac8dcf91d61
Justin van Gemen <20277@ma-web.nl>
2016-09-21 14:43:22 +0100

    Updating test scene

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -6,7 +6,7 @@ public class MoveScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Translate(5f * Time.deltaTime, 0f, 0f);
         }
     }
