4d2501d637292cb6c0603c853f5267758517e232
Justin van Gemen <20277@ma-web.nl>
2016-09-21 14:33:00 +0100

    Movescript startfix

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,26 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveScript : MonoBehaviour {
+public class MoveScript : MonoBehaviour
+{
 
-    private float _timeToMove = 0.1F;
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        MoveRight();
-    }
-    private void MoveRight()
+    void Update()
     {
-        if (Time.time > _timeToMove)
-        {
-            Vector3 position = this.transform.position;
-            position.x +=2;
-            this.transform.position = position;
-            _timeToMove = 0.2f + Time.time;
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.Translate(5f * Time.deltaTime, 0f, 0f);
         }
     }
 }
