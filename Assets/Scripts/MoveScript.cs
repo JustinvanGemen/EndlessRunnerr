0a27319d8d4aa88705210f5b0a7c1151f2a18cdf
Justin van Gemen <20277@ma-web.nl>
2016-09-26 13:33:47 +0100

    Jumpdate

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -6,16 +6,20 @@ public class MoveScript : MonoBehaviour
     private Rigidbody2D rigidBody;
     [SerializeField]
     private float jump = 400;
-    
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Translate(5f * Time.deltaTime, 0f, 0f);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidBody.AddForce(Vector3.up * jump * Time.deltaTime);
+            rigidBody.AddForce(Vector2.up * jump);
 
         }
     }
