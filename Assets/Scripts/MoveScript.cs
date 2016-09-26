5bb9603a0c6070cc411a92356bcb238f7afa5972
Justin van Gemen <20277@ma-web.nl>
2016-09-26 09:07:57 +0100

    updoot

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class MoveScript : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    [SerializeField]
+    private float jump = 400;
+    
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
             transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up * jump * Time.deltaTime);
+
         }
     }
 }
