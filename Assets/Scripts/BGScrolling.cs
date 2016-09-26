5f93f9cebe0a4687f780bc60a74e984d670a20bf
jvdwijk <joeyvdwijk@gmail.com>
2016-09-26 09:39:17 +0200

    update

--- Assets/Scripts/BGScrolling.cs
+++ Assets/Scripts/BGScrolling.cs
@@ -7,16 +7,7 @@ public class BGScrolling : MonoBehaviour {
     private Vector3 BGSize = new Vector3(35.4f, 0, 0);
     //bg = 1280, 70 * 1,4
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    //19
+    //10
     void OnBecameInvisible()
     {
         int randomBackground = Random.Range(0, 4);
