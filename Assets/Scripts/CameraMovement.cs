a029564fc6c2369f3f7422106f1900ef4f5daaa9
Justin van Gemen <20277@ma-web.nl>
2016-09-30 13:27:42 +0100

    Frapsdate

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        }
+    }
+    }
