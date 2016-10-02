153304e8369c516408e6c5fffaab33bbcb631845
jvdwijk <joeyvdwijk@gmail.com>
2016-10-02 15:00:45 +0200

    update

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,26 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 //Joey vd Wijk
 public class HealthSystem : MonoBehaviour {
     [SerializeField] 
     private float _lifes = 3; //make a float for the players life, give him 3. (if you want you can change it in the editor)
+    [SerializeField]
+    private Text text;
+    private MenuScript menuScript;  //import menuscript as var.
 
+    void Start()
+    {
+        menuScript = GetComponent<MenuScript>();  //instantiate menuscript.
+    }
 	
     void OnTriggerEnter2D (Collider2D other) //if something that has trigger enabled, hits the player
     {
-        if (_lifes <= 0) //if player runs out of lifes destroy him.
-        {
-            Destroy(gameObject);
-        }
-
-        if (other.CompareTag("Obstacle")) //if player hits an obstacle destroy the obstacle and remove a life from the player.
+        if (other.CompareTag("Obstacle")) //if player hits an obstacle remove a life from the player.
         {
             _lifes -= 1;
-            //Destroy(other.gameObject);
         }
-        else if (other.CompareTag("Water"))
+        if (_lifes <= 0) //if player runs out of lifes continue.
         {
-            _lifes -= 1;
+            if (gameObject.CompareTag("Player")) //if Player 1 died change text.
+            {
+                text.text = "Player 2 Won!";
+            }
+            else //if player 2 died change text.
+            {
+                text.text = "Player 1 Won!";
+            }
+            Destroy(gameObject); //Destroy the player with no lifes.
+            Time.timeScale = 0; //pause the game.
         }
     }
 }
--- Assets/Scripts/SpikesHandler.cs
+++ Assets/Scripts/SpikesHandler.cs
@@ -6,7 +6,7 @@ public class SpikesHandler : MonoBehaviour {
     private GameObject[] spikes;
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") || other.CompareTag("Player2")) {
             spikes[0].gameObject.SetActive(false);
             spikes[1].gameObject.SetActive(true);
         }
