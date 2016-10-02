58e86368093ede6d101db64e8d561fea00321b02
jvdwijk <joeyvdwijk@gmail.com>
2016-10-02 15:49:02 +0200

    update

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+    [SerializeField]
+    private Text score;
+    [SerializeField]
+    private Text LifeScore;
+    private float lifes;
+    private HealthSystem healthSystem;
+    private float startPos;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        healthSystem = GetComponent<HealthSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LifeScore.text = "Lifes: " + healthSystem.lifes;
+        score.text = "Score: " + (transform.position.x - startPos);
+	}
+}
