b62c5d4b7cfb3b7ed5a02ad5d81a378089835f80
jvdwijk <joeyvdwijk@gmail.com>
2016-10-03 09:25:09 +0200

    update

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -16,14 +16,30 @@ public class MoveScript : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1, 1 << LayerMask.NameToLayer("Platform"));
-
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            transform.Translate(5f * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (gameObject.CompareTag("Player"))
         {
-            rigidBody.AddForce(Vector2.up * jump);
-            isGrounded = false;
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(5f * Time.deltaTime, 0f, 0f);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+            {
+                rigidBody.AddForce(Vector2.up * jump);
+                isGrounded = false;
+            }
         }
+        else if (gameObject.CompareTag("Player2"))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(5f * Time.deltaTime, 0f, 0f);
+            }
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                rigidBody.AddForce(Vector2.up * jump);
+                isGrounded = false;
+            }
+        }
+        
     }
 }
