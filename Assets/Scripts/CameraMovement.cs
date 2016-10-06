3d9680f4885b6c720a92cad644326508510ce5fc
Justin van Gemen <20277@ma-web.nl>
2016-10-06 08:58:07 +0100

    Commit Update :)

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
+    private float speed;
 	// Use this for initialization
 	void Start () {
-	
+        speed = 3f;
 	}
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(5f * Time.deltaTime, 0f, 0f);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        //}
     }
     }
