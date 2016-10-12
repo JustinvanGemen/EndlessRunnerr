21e827bc97afef6657cc07f9e53f7b22e94ae1df
Justin van Gemen <20277@ma-web.nl>
2016-10-12 16:19:38 +0100

    Help

--- Assets/Scripts/BorderControl.cs
+++ Assets/Scripts/BorderControl.cs
@@ -5,18 +5,23 @@ public class BorderControl : MonoBehaviour {
 
     [SerializeField]
     private GameObject _otherPlayer;
+    [SerializeField]
+    private GameObject _camera;
+    private CameraMovement _camMove;
+    private float _camSpeed;
 
 	void Start () {
-        Debug.Log(_otherPlayer.transform.position.x);
+        
+        _camMove = _camera.GetComponent<CameraMovement>();
     }
 	
 	void OnTriggerStay2D(Collider2D other) {
 	    if (_otherPlayer.transform.position.x < transform.position.x)
         {
             
-            if (transform.position.x > Camera.main.transform.position.x)
+            while (other.CompareTag("CamFast"))
             {
-                Debug.Log("jkhjfeg");
+                _camMove.speed = _camMove.speed * 1.01f;
             }
         }
 	}
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-    private float speed;
+    private float _speed;
+    public float speed
+    {
+        get
+        {
+            return _speed;
+        }
+        set
+        {
+            _speed = value;
+        }
+    }
 	// Use this for initialization
 	void Start () {
-        speed = 3f;
+        _speed = 3f;
 	}
 
     // Update is called once per frame
@@ -14,7 +25,7 @@ public class CameraMovement : MonoBehaviour {
     {
         if (Time.time > 2)
         {
-            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+            transform.Translate(_speed * Time.deltaTime, 0f, 0f);
         }
     }
     }
