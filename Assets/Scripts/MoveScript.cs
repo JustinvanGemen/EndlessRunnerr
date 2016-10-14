c74ca011439e70ae310fe2a785749f46e6e1a88d
Justin van Gemen <20277@ma-web.nl>
2016-10-14 12:58:32 +0100

    Animaties werken

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -8,9 +8,12 @@ public class MoveScript : MonoBehaviour
     private float jump = 400;
     private bool isGrounded = true;
     private bool waitOnJump = true;
+    [SerializeField]
+    private Animator anim;
 
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -22,14 +25,21 @@ public class MoveScript : MonoBehaviour
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.Translate(5f * Time.deltaTime, 0f, 0f);
+                anim.SetBool("walk", true);
             }
+            if (Input.GetKey(KeyCode.RightArrow) == false && Input.GetKey(KeyCode.UpArrow) == false)
+            {
+                anim.SetBool("walk", false);
+            }
+
             if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded && waitOnJump)
             {
+                anim.SetTrigger("jump");
                 rigidBody.AddForce(Vector2.up * jump);
                 isGrounded = false;
-                Debug.Log("afs");
                 waitOnJump = false;
                 Invoke("WaitOnJump", 0.1f);
+                
             }
         }
         else if (gameObject.CompareTag("Player2"))
@@ -37,13 +47,22 @@ public class MoveScript : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 transform.Translate(5f * Time.deltaTime, 0f, 0f);
+                anim.SetBool("walk", true);
             }
+            if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.D) == false)
+            {
+                anim.SetBool("walk", false);
+
+            }
+
             if (Input.GetKeyDown(KeyCode.W) && isGrounded && waitOnJump)
             {
+                anim.SetTrigger("jump");
                 rigidBody.AddForce(Vector2.up * jump);
                 isGrounded = false;
                 waitOnJump = false;
                 Invoke("WaitOnJump", 0.1f);
+                
             }
         }
         
