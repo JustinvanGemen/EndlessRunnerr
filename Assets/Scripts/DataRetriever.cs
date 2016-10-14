8933ec92234a510e047a0e88b4d3614fe22a5c39
jvdwijk <joeyvdwijk@gmail.com>
2016-10-14 22:27:05 +0200

    update

--- Assets/Scripts/DataRetriever.cs
+++ Assets/Scripts/DataRetriever.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 public class DataRetriever : MonoBehaviour {
     [SerializeField]
     private Text info;
-    private string[] naam;
+    private string[] scores;
 	// Use this for initialization
 	IEnumerator Start () {
         WWW dataInformation = new WWW("http://jvdwijk.com/PHP/index.php");
         yield return dataInformation;
         string textData = dataInformation.text;
         Debug.Log(textData);
-        textData.Split(";"[0]);
-        info.text = textData;
+        scores = textData.Split(";"[0]);
+        info.text = scores[0] + "\n" + scores[1] + "\n" + scores [2] + "\n" + scores[3] + "\n" + scores[4];
 
 	}
 	
