cc7cdc330b8a1ee1d1bca20019fcf9efa9a6e8e2
jvdwijk <joeyvdwijk@gmail.com>
2016-10-14 22:08:53 +0200

    update

---
+++ Assets/Scripts/DataRetriever.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DataRetriever : MonoBehaviour {
+    [SerializeField]
+    private Text info;
+    private string[] naam;
+	// Use this for initialization
+	IEnumerator Start () {
+        WWW dataInformation = new WWW("http://jvdwijk.com/PHP/index.php");
+        yield return dataInformation;
+        string textData = dataInformation.text;
+        Debug.Log(textData);
+        textData.Split(";"[0]);
+        info.text = textData;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
