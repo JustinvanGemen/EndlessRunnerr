a64ea1a9662a44306f938170dbb74f659217e7fc
Justin van Gemen <20277@ma-web.nl>
2016-10-14 10:58:23 +0100

    Tutorial scene + Kill Border

--- Assets/Scripts/BorderControl.cs
+++ Assets/Scripts/BorderControl.cs
@@ -8,13 +8,23 @@ public class BorderControl : MonoBehaviour {
     [SerializeField]
     private GameObject _camera;
     private CameraMovement _camMove;
+    private HealthSystem _hSys;
     private float _camSpeed;
-    private bool fast;
-    private bool slow;
+    private bool _fast;
+    private bool _slow;
+    private float _teleport;
+    private Vector3 _thisPlayer3;
 
-	void Start () {
-        
+
+    void Start () {
+        _thisPlayer3 = transform.position;
         _camMove = _camera.GetComponent<CameraMovement>();
+        _hSys = this.GetComponent<HealthSystem>();
+    }
+
+    void update()
+    {
+        _thisPlayer3 = transform.position;
     }
 	
 	void OnTriggerStay2D(Collider2D other) {
@@ -23,32 +33,49 @@ public class BorderControl : MonoBehaviour {
             
             if (other.CompareTag("CamFast"))
             {
-                fast = true;
-                _camMove.speed = 3.1f;
+                _fast = true;
+                _camMove.speed = 5f;
                 StartCoroutine(Timer());
+                Debug.Log("Fast");
             }
             if(other.CompareTag("CamSlow"))
                 {
-                slow = true;
-                _camMove.speed = 2.9f;
+                _slow = true;
+                _camMove.speed = 1.5f;
                 StartCoroutine(Timer());
+                Debug.Log("Slow");
             }
-           
         }
-	}
+
+        if (other.CompareTag("CamKill"))
+        {
+            Debug.Log("Life lost wall");
+            _hSys.lifes -= 1f;
+            _teleport = _otherPlayer.transform.position.x - transform.position.x;
+            _thisPlayer3.x = _thisPlayer3.x + _teleport;
+            _thisPlayer3.y += 5;
+            transform.position = _thisPlayer3;
+
+        }
+
+    }
+
+
 
     IEnumerator Timer()
     {
         yield return new WaitForSeconds(2f);
-        if (fast)
+        if (_fast)
         {
             _camMove.speed = 3;
-            fast = false;
+            _fast = false;
+            Debug.Log("FastEnd");
         }
-        if (slow)
+        if (_slow)
         {
             _camMove.speed = 3;
-            slow = false;
+            _slow = false;
+            Debug.Log("SlowEnd");
         }
     }
 
